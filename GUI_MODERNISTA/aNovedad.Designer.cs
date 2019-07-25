5b2e310c250507dc0f84bafecf8fcea1e70afc67
carlosrodriguez04 <carlosrodriguezbautista@gmail.com>
2019-07-24 19:19:32 -0500

    15

--- GUI_MODERNISTA/aNovedad.Designer.cs
+++ GUI_MODERNISTA/aNovedad.Designer.cs
@@ -126,12 +126,12 @@
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(195, 97);
+            this.label1.Location = new System.Drawing.Point(644, 35);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(185, 31);
+            this.label1.Size = new System.Drawing.Size(406, 69);
             this.label1.TabIndex = 16;
             this.label1.Text = "NOVEDADES";
             // 
